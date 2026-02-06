7fdc85fec183b1d264d12370fa161ab3634e25f6
Johan Tidén <johan.tiden@looklet.com>
2026-02-06 09:56:35 +0100

    Add bobelectronic green circuits to scrap recycling output

--- data.lua
+++ data.lua
@@ -6,3 +6,9 @@ angelsmods = angelsmods or {}
 data.raw["tile"]["oil-ocean-shallow"].fluid = "crude-oil"
 data.raw["tile"]["oil-ocean-deep"].fluid = "crude-oil"
 
+-- Many Fulgora recipes require electronic-circuit but there is no way to get them on Fulgora. 
+if mods["bobelectronics"] then
+    local scrap_results = data.raw["recipe"]["scrap-recycling"].results
+    table.insert(scrap_results,
+        {type = "item", name = "electronic-circuit",        amount = 1, probability = 0.2, show_details_in_recipe_tooltip = false})
+end
